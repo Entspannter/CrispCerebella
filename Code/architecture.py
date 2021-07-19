366d844fdcb4c913c18ca4536b7e655c984f1e0e
Alinakrich <alkr6593@gmail.com>
2021-07-19 13:41:11 +0200

    add comments

--- Code/architecture.py
+++ Code/architecture.py
@@ -26,8 +26,8 @@ class LSTMStacked(HyperModel):
                         units=hp.Int(
                             'Units_LSTM_1',
                             min_value=32,
-                            max_value=128,
-                            step=16,
+                            max_value=512,
+                            step=32,
                             default=128
                         ),
                        input_shape=self.input_shape,
@@ -51,8 +51,8 @@ class LSTMStacked(HyperModel):
                         units=hp.Int(
                             'Units_LSTM_2',
                             min_value=32,
-                            max_value=128,
-                            step=16,
+                            max_value=512,
+                            step=32,
                             default=128
                         )
                     )
@@ -76,8 +76,8 @@ class LSTMStacked(HyperModel):
                 units=hp.Int(
                     'units',
                     min_value=32,
-                    max_value=128,
-                    step=16,
+                    max_value=512,
+                    step=32,
                     default=128
                 ),
                 activation=hp.Choice(
@@ -142,7 +142,7 @@ class ConvLSTM(HyperModel):
                 step=0.05,
             ))
         )
-        # Flatten the output tensor from previous layer
+        # # remove all of the dimensions of the inout tensor, except for one
         model.add(Flatten())
 
         # define the number if units and the activation function
@@ -163,6 +163,7 @@ class ConvLSTM(HyperModel):
                 )
             )
         )
+        # define the dropout rate so that it can be optimized in the hyperparameters tuning step
         model.add(
             Dropout(
                 rate=hp.Float(
@@ -174,8 +175,10 @@ class ConvLSTM(HyperModel):
                 )
             )
         )
+
         model.add(Dense(self.n_outputs, activation='softmax'))
 
+        # define the learning rate so that it can be optimized in the hyperparameters tuning step
         model.compile(
             optimizer=keras.optimizers.Adam(
                 hp.Float(
@@ -200,6 +203,7 @@ class CNNLSTM(HyperModel):
     def build(self, hp):
 
         model = keras.Sequential()
+
         model.add(TimeDistributed(
             Conv1D(
                 filters=hp.Choice(
@@ -213,6 +217,8 @@ class CNNLSTM(HyperModel):
             )
           )   
         )
+        # define the the number of output filters so that it can be optimized in the hyperparameters tuning
+        # TimeDistributed layer apply the same layer to several inputs.
         model.add(TimeDistributed(
             Conv1D(
                 filters=hp.Choice(
@@ -226,6 +232,7 @@ class CNNLSTM(HyperModel):
           )   
         )
 
+        # define the dropout rate so that it can be optimized in the hyperparameters tuning step
         model.add(TimeDistributed(Dropout(rate=hp.Float(
                 'dropout_1',
                 min_value=0.0,
@@ -236,11 +243,16 @@ class CNNLSTM(HyperModel):
            )
           )
         )
-      
+
+        # MaxPooling1D downsamples the input representation by taking
+        # the maximum value over a spatial window of size pool_size
+
         model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
 
+        # remove all of the dimensions of the inout tensor, except for one
         model.add(TimeDistributed(Flatten()))
 
+        # define the number if units so that it can be optimized in the hyperparameters tuning step
         model.add(
             LSTM(
                 units=hp.Int(
@@ -253,6 +265,7 @@ class CNNLSTM(HyperModel):
             )
         )
 
+        # define the dropout rate so that it can be optimized in the hyperparameters tuning step
         model.add(
             Dropout(
                 rate=hp.Float(
@@ -265,6 +278,8 @@ class CNNLSTM(HyperModel):
             )
         )
 
+        # define the number if units and the activation function
+        # so that it can be optimized in the hyperparameters tuning step
         model.add(
             Dense(
                 units=hp.Int(
@@ -283,6 +298,7 @@ class CNNLSTM(HyperModel):
         )
         model.add(Dense(self.n_outputs, activation='softmax'))
 
+        # define the learning rate so that it can be optimized in the hyperparameters tuning step
         model.compile(
             optimizer=keras.optimizers.Adam(
                 hp.Float(
