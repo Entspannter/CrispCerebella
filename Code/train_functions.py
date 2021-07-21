aa3fd8479959f20fd34c3520b452df90d11eaa62
Alinakrich <46444279+Alinakrich@users.noreply.github.com>
2021-07-21 10:53:26 +0200

    added seed

--- Code/train_functions.py
+++ Code/train_functions.py
@@ -15,16 +15,11 @@ np.random.seed(seed_value)
 
 # 4. Set the `tensorflow` pseudo-random generator at a fixed value
 import tensorflow as tf
-tf.random.set_seed(seed_value)
-# for later versions: 
-# tf.compat.v1.set_random_seed(seed_value)
+#tf.random.set_seed(seed_value)
+#for later versions: 
+tf.compat.v1.set_random_seed(seed_value)
 
 # 5. Configure a new global `tensorflow` session
-#from keras import backend as K
-#session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-#sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-#K.set_session(sess)
-# for later versions:
 session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
 sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
 tf.compat.v1.keras.backend.set_session(sess)
