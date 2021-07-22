beded686d33c12d32f811c63e2581961291b988f
Alinakrich <46444279+Alinakrich@users.noreply.github.com>
2021-07-22 16:28:34 +0200

    Update train_functions.py

--- Code/train_functions.py
+++ Code/train_functions.py
@@ -96,8 +96,8 @@ def k_fold_cv(x, y, model_class, model_name, input_shape, num_outputs, max_trial
         print('Best epoch: %d' % (best_epoch,))
 
         # re-instantiate the hypermodel and train it with the optimal number of epochs from above
-        hypermodel = model_class(input_shape, num_outputs)
-        new_best_model = hypermodel.build(best_hp)
+        hypermodel_new = model_class(input_shape, num_outputs)
+        new_best_model = hypermodel_new.build(best_hp)
         new_best_model.fit(X_train, y_train, epochs=best_epoch, validation_split=0.2)
 
         # evaluate the model 
