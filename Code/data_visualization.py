986387e2e4b3a0d082494b932f7b88cf07ec1013
Alinakrich <46444279+Alinakrich@users.noreply.github.com>
2021-07-22 18:49:35 +0200

    Update data_visualization.py

--- Code/data_visualization.py
+++ Code/data_visualization.py
@@ -4,7 +4,7 @@ from numpy import unique
 import matplotlib.pyplot as plt
 import seaborn as sns
 from numpy import vstack
-from data_loading import *
+from CrispCerebella.Code.data_loading import *
 from IPython.display import display
 
 
