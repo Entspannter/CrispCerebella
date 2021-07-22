6adf2e53ec67b52e945d891253be6202034f40ed
Linea-code <info@lineaschmidt.de>
2021-07-22 10:07:06 +0200

    Add Seed functions to train_functions.py

--- Code/train_functions.py
+++ Code/train_functions.py
@@ -3,7 +3,32 @@ from tensorflow.keras.utils import to_categorical
 import keras_tuner
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import KFold
+import tensorflow as tf
+import os
+import numpy as np
+import random
 
+SEED = 42
+
+def set_seeds(seed=SEED):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+
+def set_global_determinism(seed=SEED):
+    set_seeds(seed=seed)
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
+
+
+# Call the above function with seed value
+set_global_determinism(seed=SEED)
 
 # load the dataset, returns train and test X and y elements
 def transform_dataset(x_train, y_train, x_test, y_test):
