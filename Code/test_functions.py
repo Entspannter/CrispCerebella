0ff6a7e6c991c09e88fa1c9e843c4e97374dd10a
Alinakrich <46444279+Alinakrich@users.noreply.github.com>
2021-07-22 18:54:47 +0200

    Update test_functions.py

--- Code/test_functions.py
+++ Code/test_functions.py
@@ -4,8 +4,8 @@ import seaborn as sns
 from sklearn.metrics import classification_report
 from numpy import unique
 
-
-# function to create a confusion matrix and a heatmap
+def create_heatmap(best_model, x_test, y_test, y_labels)
+    # function to create a confusion matrix and a heatmap
     # create prediction based on the input dataset
     # convert the output from categorical to numerical
     prediction = np.argmax(best_model.predict(x_test), axis=-1)
